a73fe8a5b369dd4d5716fd12ed2f3e3bf727b4f2
Luna del Valle <luna7111@tuta.io>
2025-03-03 18:05:31 +0100

    Added philo cycle functions, but the functions inside them are not made yet.

--- src/thread_functions.c
+++ src/thread_functions.c
@@ -6,7 +6,7 @@
 /*   By: ldel-val <ldel-val@student.42madrid.com>  |  |           *           */
 /*                                                 \  '.___.;       +         */
 /*   Created: 2025/02/28 18:53:59 by ldel-val       '._  _.'   .        .     */
-/*   Updated: 2025/02/28 19:01:41 by ldel-val          ``                     */
+/*   Updated: 2025/03/03 18:05:11 by ldel-val          ``                     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,34 @@
 
 void	left_handed_philo(t_philo *philo)
 {
+	while (philo->iterations != 0)
+	{
+		take_left_fork(philo);
+		take_right_fork(philo);
+		eat(philo);
+		leave_right_fork(philo);
+		leave_left_fork(philo);
+		sleep(philo);
+		think(philo);
+		if (philo->iterations != INFINITE)
+			philo->iterations --;
+	}
 }
 
 void	right_handed_philo(t_philo *philo)
 {
+	while (philo->iterations != 0)
+	{
+		take_right_fork(philo);
+		take_left_fork(philo);
+		eat(philo);
+		leave_left_fork(philo);
+		leave_right_fork(philo);
+		sleep(philo);
+		think(philo);
+		if (philo->iterations != INFINITE)
+			philo->iterations --;
+	}
 }
 
 void	*philo_thread(void *philo)
